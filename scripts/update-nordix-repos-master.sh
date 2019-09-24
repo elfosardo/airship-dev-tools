056101062390c97713e5b405f779e688ae11b1e2
Mika Koskimaki <mika.koskimaki@est.tech>
2019-09-24 10:45:33 +0000

    ci: work in progress, sync nordix repos

--- scripts/update-nordix-repos-master.sh
+++ scripts/update-nordix-repos-master.sh
@@ -1,30 +1,73 @@
 #!/bin/bash
-
-#Call with
-#./update.sh <repo> <branch>
-#for example
-#./update.sh cluster-api update-branch
-
 set -ue
 
+#------------------------------------------
+# Workflow:
+# -clone repos to jenkins if not there
+# -rebase jenkins local repos from upstream
+# -push jenkins local repos to Nordix forks
+#------------------------------------------
+
 SCRIPTPATH="$(dirname "$(readlink -f "${0}")")"
+#LOCAL_REPO_PATH="${GOPATH}/src"
+LOCAL_REPO_PATH="${WORKSPACE}/nordix_update_repos"
+
+CAPIPB_REPO="https://github.com/metal3-io/cluster-api-provider-baremetal.git"
+CAPI_REPO="https://github.com/kubernetes-sigs/cluster-api.git"
+BMO_REPO="https://github.com/metal3-io/baremetal-operator.git"
+M3DOCS_REPO="https://github.com/metal3-io/metal3-docs.git"
+M3DEVENV_REPO="https://github.com/metal3-io/metal3-dev-env.git"
+
+NORDIX_CAPIPB_REPO="https://github.com/Nordix/cluster-api-provider-baremetal.git"
+NORDIX_CAPI_REPO="https://github.com/Nordix/cluster-api.git"
+NORDIX_BMO_REPO="https://github.com/Nordix/baremetal-operator.git"
+NORDIX_M3DOCS_REPO="https://github.com/Nordix/metal3-docs.git"
+NORDIX_M3DEVENV_REPO="https://github.com/metal3-io/metal3-dev-env.git"
+
+LOCAL_CAPIPB_REPO="${LOCAL_REPO_PATH}/cluster-api-provider-baremetal"
+LOCAL_CAPI_REPO="${LOCAL_REPO_PATH}/cluster-api"
+LOCAL_BMO_REPO="${LOCAL_REPO_PATH}/baremetal-operator"
+LOCAL_M3DOCS_REPO="${LOCAL_REPO_PATH}/metal3-docs"
+LOCAL_M3DEVENV_REPO="${LOCAL_REPO_PATH}/metal3-dev-env"
 
 pushd "${SCRIPTPATH}"
 cd ..
 
-UPDATE_REPO="${1:-go/src/sigs.k8s.io/cluster-api metal3-dev-env metal3-docs go/src/github.com/metal3-io/cluster-api-provider-baremetal go/src/github.com/metal3-io/baremetal-operator}"
+UPDATE_REPO="${1:-${LOCAL_CAPIPB_REPO} ${LOCAL_CAPI_REPO} ${LOCAL_BMO_REPO} ${LOCAL_M3DOCS_REPO} ${LOCAL_M3DEVENV_REPO}}"
 UPDATE_BRANCH="${2:-master}"
+UPSTREAM_REPO="${3:-${CAPIPB_REPO} ${CAPI_REPO} ${BMO_REPO} ${M3DOCS_REPO} ${M3DEVENV_REPO}}"
+NORDIX_REPO="${4:-${NORDIX_CAPIPB_REPO} ${NORDIX_CAPI_REPO} ${NORDIX_BMO_REPO} ${NORDIX_M3DOCS_REPO} ${NORDIX_M3DEVENV_REPO}}"
+
+# clone upstream repos to jenkins if not found
+i=0
+locarray=(${UPDATE_REPO})
+upsarray=(${UPSTREAM_REPO})
+
+pushd ${LOCAL_REPO_PATH}
+
+for index in ${UPDATE_REPO}
+do
+    if [ ! -d "${locarray[$i]}" ]; then
+      echo "CLONE "${upsarray[$i]}""
+      git clone ${upsarray[$i]}
+    fi
+i=$(($i+1));
+done
+
+cd -
 
 for repo in ${UPDATE_REPO}
 do
+  echo "PWD ${PWD}"
   echo "Updating ${repo}"
   pushd "${repo}"
   # Update "master" on Nordix
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
   git checkout "${UPDATE_BRANCH}"
-  git fetch upstream
-  git rebase upstream/master
-  git push
+  # origin points to upstream repos
+  git fetch origin
+  git rebase origin/master
+  git push --repo="${NORDIX_REPO}"
   git checkout "${BRANCH}"
   popd
   echo -e "\n"
